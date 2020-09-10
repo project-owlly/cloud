f6cc3ab10a5f574c0c9fe8722c8fe838e52bcea2
peterpeterparker <david.dalbusco@outlook.com>
2020-09-10 13:35:53 +0200

    feat(#1): type definitions and mock data

--- functions/src/request/owlly/owlly.get.ts
+++ functions/src/request/owlly/owlly.get.ts
@@ -2,14 +2,20 @@ import * as functions from 'firebase-functions';
 
 import * as cors from 'cors';
 
-export function getOwlly(request: functions.Request, response: functions.Response<any>) {
+import {Owlly} from '../../types/owlly';
+import {RequestError} from '../../types/request.error';
+
+export function getOwlly(request: functions.Request, response: functions.Response<Owlly | RequestError>) {
     const corsHandler = cors({origin: true});
 
     corsHandler(request, response, () => {
         try {
-            // TODO
             response.json({
-                todo: 'todo'
+                id: `28c72f42-5daa-4a28-b39d-b51b203c3740`,
+                title: 'Sichere Velorouten für Zürich',
+                description: 'Mehr als die Hälfte der Velofahrenden gibt an, dass sie sich im Strassenverkehr häufig unsicher fühlen.',
+                link: `/initiative/sichere-velorouten-fuer-zuerich`,
+                organisation: 'Initiativkomitee «Sichere Velorouten für Zürich»'
             });
         } catch (err) {
             response.status(500).json({
