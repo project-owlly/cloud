a5001ed4f91246c0cb03c74cf7eb90a7e36c8d54
Sandro Scalco <info@sandrodesign.ch>
2020-09-15 07:19:13 +0200

    new id for owlly

--- functions/src/request/owlly/owlly.get.ts
+++ functions/src/request/owlly/owlly.get.ts
@@ -11,7 +11,7 @@ export function getOwlly(request: functions.Request, response: functions.Respons
     corsHandler(request, response, () => {
         try {
             response.json({
-                id: `28c72f42-5daa-4a28-b39d-b51b203c3740`,
+                id: `mkro4noxKW9CNGE7mGFE`,
                 title: 'Sichere Velorouten für Zürich',
                 description: 'Mehr als die Hälfte der Velofahrenden gibt an, dass sie sich im Strassenverkehr häufig unsicher fühlen.',
                 link: `/initiative/sichere-velorouten-fuer-zuerich`,
