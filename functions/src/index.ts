b2b5ed2ec9e8d22e8a4096aea3a2499556e71978
Sandro Scalco <sandro.scalco@liitu.ch>
2020-09-16 22:09:42 +0200

    pdf function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,3 +7,7 @@ import {getOwlly} from './request/owlly/owlly.get';
 export const owlly = functions.https.onRequest(getOwlly);
 
 export const mailbox = functions.https.onRequest((req: any, res: any) => {});
+
+export const generatePDF = functions.https.onRequest((req: any, res: any) => {
+  //Todo JONATHAN
+});
