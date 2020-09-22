ce1b05508a5bdd8ac0eedcc78aef1e9e3c516c62
jonarivado <jonathan.dotzler@gmail.com>
2020-09-22 13:01:24 +0200

    rudimentary pdf generator

--- functions/src/request/pdf/pdf.post.ts
+++ functions/src/request/pdf/pdf.post.ts
@@ -3,5 +3,29 @@ import * as functions from 'firebase-functions';
 export function postGeneratePdf(request: functions.Request, response: functions.Response<any>) {
   // TODO JONATHAN
 
-  response.json({result: 'success'});
+var user = {
+  email: '',
+  name: ''
+
+};
+
+user.email = request.body.email;
+user.name = request.body.name;
+
+
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+const doc = new PDFDocument();
+
+doc.pipe(fs.createWriteStream('file.pdf'));
+
+
+doc.text('Hello World! This is the first test for Owlly!', 100, 100);
+doc.text(user.email);
+doc.text(user.name);
+
+doc.end();
+
+
+  response.json({result: 'successful'});
 }
