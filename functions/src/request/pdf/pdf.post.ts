992d5165161bd62e008cebed010c25b3eefd095d
jonarivado <jonathan.dotzler@gmail.com>
2020-09-22 18:31:38 +0200

    Update pdf.post.ts

--- functions/src/request/pdf/pdf.post.ts
+++ functions/src/request/pdf/pdf.post.ts
@@ -1,31 +1,44 @@
 import * as functions from 'firebase-functions';
 
-export function postGeneratePdf(request: functions.Request, response: functions.Response<any>) {
+import * as cors from 'cors';
+
+export function postGeneratePdf(request: functions.Request, response: functions.Response < any > ) {
   // TODO JONATHAN
 
-var user = {
-  email: '',
-  name: ''
+  const corsHandler = cors({
+    origin: true
+  });
 
-};
-
-user.email = request.body.adress;
-user.name = request.body.vorname;
+  corsHandler(request, response, () => {
 
 
-const PDFDocument = require('pdfkit');
-const fs = require('fs');
-const doc = new PDFDocument();
+    var user = {
+      email: '',
+      name: ''
+    
+    };
+    
+    user.email = request.body.adress;
+    user.name = request.body.vorname;
+    
+    
+    const PDFDocument = require('pdfkit');
+    const fs = require('fs');
+    const doc = new PDFDocument();
+    
+    doc.pipe(fs.createWriteStream('file.pdf'));
+    
+    
+    doc.text('Hello World! This is the first test for Owlly!', 100, 100);
+    doc.text(user.email);
+    doc.text(user.name);
+    
+    doc.end();
+    
+    
+      response.json({result: 'successful'});
 
-doc.pipe(fs.createWriteStream('file.pdf'));
 
 
-doc.text('Hello World! This is the first test for Owlly!', 100, 100);
-doc.text(user.email);
-doc.text(user.name);
-
-doc.end();
-
-
-  response.json({result: 'successful'});
+  });
 }
