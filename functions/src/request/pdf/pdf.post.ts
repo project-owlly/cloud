c66a274829fcc0ccd47dbc4a59c1599ecd1f99d2
peterpeterparker <david.dalbusco@outlook.com>
2020-10-10 09:32:01 +0200

    feat: add owllyId to PDF metadata

--- functions/src/request/pdf/pdf.post.ts
+++ functions/src/request/pdf/pdf.post.ts
@@ -1,26 +1,36 @@
 import * as functions from 'firebase-functions';
 
-import * as cors from 'cors';
-
 import * as PDFDocument from 'pdfkit';
 
 // TODO JONATHAN
 
-export function postGeneratePdf(request: functions.Request, response: functions.Response<any>) {
-  const corsHandler = cors({origin: true});
+interface OwllyDocumentInfo extends PDFKit.DocumentInfo {
+  OwllyId: string;
+}
 
-  corsHandler(request, response, () => {
-    const doc: PDFKit.PDFDocument = new PDFDocument();
+export function getGeneratePdf(request: functions.Request, response: functions.Response<any>) {
+  const owllyId: string | undefined = request.params.owllyId;
 
-    doc.text('Hello World! This is the first test for Owlly!', 100, 100);
+  if (!owllyId) {
+    response.status(500).json({
+      error: 'OwllyId not provided',
+    });
 
-    // https://stackoverflow.com/a/54355501/5404186
+    return;
+  }
 
-    response.setHeader('Content-disposition', 'attachment; filename="hello.pdf"');
-    response.setHeader('Content-type', 'application/pdf');
+  const doc: PDFKit.PDFDocument = new PDFDocument();
 
-    doc.pipe(response.status(200));
+  (doc.info as OwllyDocumentInfo).OwllyId = owllyId;
 
-    doc.end();
-  });
+  doc.text('Hello World! This is the first test for Owlly!', 100, 100);
+
+  // https://stackoverflow.com/a/54355501/5404186
+
+  response.setHeader('Content-disposition', 'attachment; filename="hello.pdf"');
+  response.setHeader('Content-type', 'application/pdf');
+
+  doc.pipe(response.status(200));
+
+  doc.end();
 }
