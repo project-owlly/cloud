2b669db95f0ca869c410cff15d800d64845b02c0
peterpeterparker <david.dalbusco@outlook.com>
2020-10-10 08:37:05 +0200

    feat: use typescript definition and write PDF output to http response without file system

--- functions/src/request/pdf/pdf.post.ts
+++ functions/src/request/pdf/pdf.post.ts
@@ -2,34 +2,25 @@ import * as functions from 'firebase-functions';
 
 import * as cors from 'cors';
 
-export function postGeneratePdf(request: functions.Request, response: functions.Response<any>) {
-  // TODO JONATHAN
+import * as PDFDocument from 'pdfkit';
 
-  const corsHandler = cors({
-    origin: true,
-  });
+// TODO JONATHAN
+
+export function postGeneratePdf(request: functions.Request, response: functions.Response<any>) {
+  const corsHandler = cors({origin: true});
 
   corsHandler(request, response, () => {
-    var user = {
-      email: '',
-      name: '',
-    };
-
-    user.email = request.body.adress;
-    user.name = request.body.vorname;
-
-    const PDFDocument = require('pdfkit');
-    const fs = require('fs');
     const doc = new PDFDocument();
 
-    doc.pipe(fs.createWriteStream('file.pdf'));
-
     doc.text('Hello World! This is the first test for Owlly!', 100, 100);
-    doc.text(user.email);
-    doc.text(user.name);
+
+    // https://stackoverflow.com/a/54355501/5404186
+
+    response.setHeader('Content-disposition', 'attachment; filename="hello.pdf"');
+    response.setHeader('Content-type', 'application/pdf');
+
+    doc.pipe(response.status(200));
 
     doc.end();
-
-    response.json({result: 'successful'});
   });
 }
