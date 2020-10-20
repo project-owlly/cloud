fc5d55b6d790b550467e03723974c5cdc2598f16
peterpeterparker <david.dalbusco@outlook.com>
2020-10-20 13:58:32 +0200

    feat: try to generate svg img in PDF

--- functions/src/request/pdf/utils/pdf.utils.ts
+++ functions/src/request/pdf/utils/pdf.utils.ts
@@ -3,18 +3,18 @@
 
 import * as PDFDocument from 'pdfkit';
 
-// import {owllyTop} from './images/owlly_top';
+import {owllyTop} from './images/owlly_top';
 
-// const SVGtoPDF = require('svg-to-pdfkit');
+const SVGtoPDF = require('svg-to-pdfkit');
 
 export async function generatePDFDoc(): Promise<PDFKit.PDFDocument> {
   const doc: PDFKit.PDFDocument = new PDFDocument();
 
-  // SVGtoPDF(doc, owllyTop, (doc.page.width - 277 * 0.22) / 2, -15, {scale: 0.22});
+  SVGtoPDF(doc, owllyTop, (doc.page.width - 277 * 0.22) / 2, -15, {scale: 0.22});
 
   // doc.fillColor('red').font('./fonts/Lato-Thin.ttf').fontSize(16).text('KANTONALE VOLKSINITIATIVE', 0, 39.5, {align: 'center'});
 
-  doc.text('Hello World! This is the first test for Owlly!', 100, 100);
+  doc.text('Hello World 2! This is the first test for Owlly!', 100, 100);
 
   return doc;
 }
