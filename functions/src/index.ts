07c87d70a492a3670ff375ea6db25afa587d5218
Sandro Scalco <sandro.scalco@liitu.ch>
2020-11-09 10:38:02 +0100

    eidlogin

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,26 +4,34 @@ import 'firebase-functions/lib/logger/compat';
 import {getOwlly} from './request/owlly/owlly.get';
 import {mailboxGet} from './request/mailbox/mailbox.get';
 import {callGeneratePdfUrl} from './request/pdf/pdf.call';
+
 import {callOIDAuthUrl} from './request/oidc/getOIDC';
 import {getEidData} from './request/oidc/getEidData';
+
+import {callOIDAuthUrlLogin} from './request/oidc/getOIDCLogin';
+import {callEidLogin} from './request/oidc/loginEid';
 
 import {readMailbox} from './scheduler/mailbox/mailbox.scheduler';
 
 import {sendNewsletterWelcomeMail} from './firestore/newsletter/newsletter.create';
 import {sendFeedbackThankYouMail} from './firestore/feedback/feedback.create';
 
-//HTTP Requests
+//HTTP Requests (EXTERNAL) - Internal = from App should be onCall..
 export const owlly = functions.region('europe-west6').https.onRequest(getOwlly);
 export const mailbox = functions.region('europe-west6').https.onRequest(mailboxGet);
 export const eidData = functions.region('europe-west6').https.onRequest(getEidData); //could be "onCall" for callable functions
 
-//scheduler
+//scheduler (Mailbox, Cleanup Documents)
 export const mailboxScheduler = functions.region('europe-west6').pubsub.schedule('every 15 minutes').onRun(readMailbox);
 
 // FIRESTORE DATABASE Listener
 export const newsletterWelcomeEmail = functions.region('europe-west6').firestore.document('/newsletter/{id}').onCreate(sendNewsletterWelcomeMail);
 export const feedbackThankyouEmail = functions.region('europe-west6').firestore.document('/feedback/{id}').onCreate(sendFeedbackThankYouMail);
 
-//onCall
+//onCall +eid Data for PDF
 export const OIDAuthUrl = functions.region('europe-west6').https.onCall(callOIDAuthUrl);
 export const generatePDF = functions.region('europe-west6').https.onCall(callGeneratePdfUrl);
+
+//onCall eID+ Login
+export const OIDAuthUrlLogin = functions.region('europe-west6').https.onCall(callOIDAuthUrlLogin);
+export const eidLogin = functions.region('europe-west6').https.onCall(callEidLogin);
