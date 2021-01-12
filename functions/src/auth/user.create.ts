ac1f374bdd45512487b1495c47ad62025a478594
Sandro Scalco <sandro.scalco@liitu.ch>
2021-01-12 20:05:41 +0100

    firebase init

--- functions/src/auth/user.create.ts
+++ functions/src/auth/user.create.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
-admin.initializeApp(functions.config().firebase);
+//admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 export function authUserCreate(user: admin.auth.UserRecord, context: functions.EventContext) {
   //update
